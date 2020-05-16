987e18f17b3cbc7e9117cda99cb3337db60c684c
aamahi <amahi.wp@gmail.com>
2020-05-17 01:56:20 +0600

    Frontend Template Mastering Compleate

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/admin/home', 'Admin@index');
 Route::get('/admin/login', 'Admin\LoginController@showLoginForm')->name('admin.login');
 Route::post('/admin/login', 'Admin\LoginController@login');
+
+//============== FRONTEND =====================================
+
+Route::get('/', 'Frontend\Index@home');
+
+//============== ADMIN =====================================
