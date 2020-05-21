44cc4dc2812a1aac699f84f3a81869ab6575991f
aamahi <amahi.wp@gmail.com>
2020-05-22 01:03:00 +0600

    Banner show in Frontend

--- app/Http/Controllers/Frontend/Index.php
+++ app/Http/Controllers/Frontend/Index.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Model\Banner;
 use App\Model\Brand;
 use App\Model\Category\HeadCategory;
 use Illuminate\Http\Request;
@@ -12,7 +13,8 @@ class Index extends Controller
     public function home(){
         $head_categories = HeadCategory::with('sub_categories')->select('id','head_category_name','category_icon','category_banner')->get();
         $brands = Brand::select('brand_logo')->orderBy('id','DESC')->get();
-        return view("frontend.content.home",compact('brands','head_categories'));
+        $banners = Banner::select('web_banner')->orderBy('id','DESC')->get();
+        return view("frontend.content.home",compact('brands','head_categories','banners'));
     }
 
 }
