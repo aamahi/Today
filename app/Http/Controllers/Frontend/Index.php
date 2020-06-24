76e361f576dbf76dc7fe74764263d7cb5dd1f76d
aamahi <amahi.wp@gmail.com>
2020-06-24 22:40:13 +0600

    Show testimonial part in frontend

--- app/Http/Controllers/Frontend/Index.php
+++ app/Http/Controllers/Frontend/Index.php
@@ -8,12 +8,14 @@ use App\Model\Brand;
 use App\Model\Cart;
 use App\Model\Category\HeadCategory;
 use App\Model\Product;
+use App\Model\Testemonial;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class Index extends Controller
 {
     public function home(){
+            $testemonials = Testemonial::all();
             $ip_address = \request()->ip();
             $carts = Cart::with('product')->where('ip_address',$ip_address)->select('id','product_id','qunt')->orderBy('id','desc')->paginate(5);
             $head_categories = HeadCategory::with('sub_categories')->select('id','head_category_name','category_icon','category_banner')->get();
@@ -23,7 +25,7 @@ class Index extends Controller
             $special_offers = Product::select('id','product_name','photo','price','discount_price','quantity','hot_deal','special_offer','today_offer')->orderBy('id','DESC')->where('special_offer',1)->get();
             $today_offers= Product::select('id','product_name','photo','price','discount_price','quantity','hot_deal','special_offer','today_offer')->orderBy('id','DESC')->where('today_offer',1)->get();
 //        $special_offers = Product::select('id','product_name','photo','price','quantity')->orderBy('id','DESC')->where('special_offer',1)->get();
-            return view("frontend.content.home",compact('brands','head_categories','banners','products','special_offers','today_offers','carts'));
+            return view("frontend.content.home",compact('brands','head_categories','banners','products','special_offers','today_offers','carts','testemonials'));
     }
 
 }
